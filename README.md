3bf119ef66835ff76f5b48342d6835249aa677ae
Mohammad Ayub <mohammad.ayub@imagination.com>
2019-09-23 07:34:51 +0100

    added authentication implementation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # userauthapp
+An Expressjs app implementing local authentication using passportjs. The backend of authentication is Mongodb.
--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -31,7 +32,7 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/secret", (req, res) => {
+app.get("/secret", isLoggedIn, (req, res) => {
     res.render("secret");
 });
 
@@ -42,8 +43,44 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
+  console.log("The post register route has been called");  
+  User.register({username: req.body.user.username}, req.body.user.password, (err, user)=>{
+      if(err){
+        console.log(err);
+        return res.render("register-error", {err:err});
+      }
+      else{
+          passport.authenticate("local")(req,res, ()=>{
+              res.redirect("/secret");
+          })
+      }
+  })
+});
+/*******************************LOGIN ROUTES ***********************************************/
+app.get("/login", (req, res) => {
   res.render("login");
 });
+// injected middleware, that defines the course of action upon success and failure
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), (req, res) => {
+});
+
+/*****************************************LOGOUT ROUTES ************************************/
+app.get("/logout", (req, res) => {
+  req.logOut();  
+  res.redirect("/");
+});
+
+/******************************************* WRITING MIDDLEWARE TO LOCKDOWN /SECRET ROUTE**********************************/
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port} 🔥`);
